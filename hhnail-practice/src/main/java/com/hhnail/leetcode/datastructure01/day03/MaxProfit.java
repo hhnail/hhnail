c526366e4a97c3e954d89ac88c1cc747200b1cec
Hhnail <779943749@qq.com>
2022-01-07 11:52:19 +0800

    [算法]leetcode算法题-数据结构-day03

---
+++ hhnail-practice/src/main/java/com/hhnail/leetcode/datastructure01/day03/MaxProfit.java
@@ -0,0 +1,22 @@
+package com.hhnail.leetcode.datastructure01.day03;
+
+public class MaxProfit {
+
+	public static void main(String[] args) {
+		// int[] arr = {7, 1, 5, 3, 6, 4};
+		int[] arr = {7, 6, 4, 3, 1};
+		System.out.println(MaxProfit.maxProfit(arr));
+
+	}
+
+	public static int maxProfit(int[] prices) {
+		int min = prices[0];
+		int max = 0;
+		for (int i = 1; i < prices.length; i++) {
+			int value = prices[i];
+			min = Math.min(min, value);
+			max = Math.max(max, (value - min));
+		}
+		return max;
+	}
+}
