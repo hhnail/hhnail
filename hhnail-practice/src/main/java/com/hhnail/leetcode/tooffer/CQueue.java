8c8c1fcbe9da3b14a96ce695f6f942e13099f741
Hhnail <779943749@qq.com>
2022-01-31 11:10:49 +0800

    [leetcode算法]动态规划，爬楼梯over；开新坑，剑指offer

--- hhnail-practice/src/main/java/com/hhnail/leetcode/tooffer/CQueue.java
+++ hhnail-practice/src/main/java/com/hhnail/leetcode/tooffer/CQueue.java
@@ -1,5 +1,7 @@
 package com.hhnail.leetcode.tooffer;
 
+import java.util.Stack;
+
 /**
  * 用两个栈实现一个队列。队列的声明如下，
  * 请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。
@@ -8,8 +10,15 @@ package com.hhnail.leetcode.tooffer;
  */
 public class CQueue {
 
+	public Stack<Integer> stack1 = new Stack<>();
+	public Stack<Integer> stack2 = new Stack<>();
+
 	public static void main(String[] args) {
+
 		CQueue c = new CQueue();
+		c.stack1.push(1);
+		System.out.println(c.stack1);
+
 	}
 
 	public CQueue() {
