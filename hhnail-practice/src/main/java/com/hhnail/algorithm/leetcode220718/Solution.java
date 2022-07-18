95c5325ba9cc0910bf2a3ba07b73b7f1aa5c2865
Hhnail <779943749@qq.com>
2022-07-18 09:21:42 +0800

    [leetcode]两个数对之间的最大乘积差

---
+++ hhnail-practice/src/main/java/com/hhnail/algorithm/leetcode220718/Solution.java
@@ -0,0 +1,21 @@
+package com.hhnail.algorithm.leetcode220718;
+
+import java.util.Arrays;
+
+/*
+1913. 两个数对之间的最大乘积差
+https://leetcode.cn/problems/maximum-product-difference-between-two-pairs/
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] testData = {5, 6, 2, 7, 4};
+        int result = maxProductDifference(testData);
+        System.out.println(result);
+    }
+
+    public static int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length - 1] * nums[nums.length - 2] - nums[0] * nums[1];
+    }
+}
