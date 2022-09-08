900d6c43cc96893473252f2b76524eb5f3521a2a
penghangwei <hhnail@163.com>
2022-09-08 13:22:37 +0800

    test Map

---
+++ hhnail-practice/src/main/java/com/hhnail/test/MapTest.java
@@ -0,0 +1,18 @@
+package com.hhnail.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+    public static void main(String[] args) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(1,3);
+        map.put(12,3);
+        map.put(12,3);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+
+        }
+    }
+}
