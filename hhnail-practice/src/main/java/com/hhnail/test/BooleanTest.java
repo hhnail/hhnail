9788bc81d2b0b2b02db31bc88e721fc0840bb59d
penghangwei <hhnail@163.com>
2022-09-08 13:44:09 +0800

    test Boolean

---
+++ hhnail-practice/src/main/java/com/hhnail/test/BooleanTest.java
@@ -0,0 +1,9 @@
+package com.hhnail.test;
+
+public class BooleanTest {
+    public static void main(String[] args) {
+        Boolean b = 1 == 1;
+        System.out.println(b);
+        System.out.println(b == true);
+    }
+}
