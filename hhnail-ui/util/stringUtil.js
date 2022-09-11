a68561f7c2cbc4934554c020049529ef47da3f8d
Hhnail <779943749@qq.com>
2022-09-11 20:20:33 +0800

    string工具类增强

--- hhnail-ui/util/stringUtil.js
+++ hhnail-ui/util/stringUtil.js
@@ -1,13 +1,24 @@
-const getTimeFromDecimal = (date) => {
+/**
+ *
+ * 将decimal类型的日期、时间转化为:\-等分隔符隔开的类型
+ * @param date
+ * @returns {string}
+ *
+ * eg
+ * separator：:   091109 -> 09:11:09
+ * separator：-   221209 -> 22-12-09
+ *
+ */
+const getTimeFromDecimal = (date,separator) => {
     let template = '0000000'
     let result = ''
     const strDate = date.toString()
-    template = template.substr(0, 6 - strDate.length) + strDate
+    template = template.substr(0, template.length - strDate.length) + strDate
     let index = 1;
     for (const tempChar of template) {
         result = result + tempChar
         if (index == 2 || index == 4) {
-            result = result + ":"
+            result = result + separator
         }
         index++
     }
