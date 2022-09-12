77d3c15923e3ddc63c3e0ff7c109e68750e4bf8b
penghangwei <hhnail@163.com>
2022-09-12 09:39:16 +0800

    map test

--- hhnail-practice/src/main/java/com/hhnail/test/MapTest.java
+++ hhnail-practice/src/main/java/com/hhnail/test/MapTest.java
@@ -6,9 +6,9 @@ import java.util.Map;
 public class MapTest {
     public static void main(String[] args) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        map.put(1,3);
-        map.put(12,3);
-        map.put(12,3);
+        map.put(1, 3);
+        map.put(12, 3);
+        map.put(12, 3);
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
 
             System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
