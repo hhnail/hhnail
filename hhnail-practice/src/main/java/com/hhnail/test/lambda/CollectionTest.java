ceb9af87f958174427cc6dfebd974d2607527372
penghangwei <hhnail@163.com>
2022-11-05 11:25:25 +0800

    lambda的filter、groupingby、map遍历

--- hhnail-practice/src/main/java/com/hhnail/test/lambda/CollectionTest.java
+++ hhnail-practice/src/main/java/com/hhnail/test/lambda/CollectionTest.java
@@ -2,12 +2,14 @@ package com.hhnail.test.lambda;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Data
@@ -16,19 +18,39 @@ public class CollectionTest {
 
     @Data
     @AllArgsConstructor
+    @ToString
     class UserInfo {
         private String id;
         private String name;
         private Integer age;
+        private String city;
     }
 
 
+    private List<UserInfo> getTestListData() {
+        List<UserInfo> list = new ArrayList<>();
+        list.add(new UserInfo("1", "张三", 29, "杭州"));
+        list.add(new UserInfo("2", "李四", 17, "深圳"));
+        list.add(new UserInfo("2", "王五", 30, "杭州"));
+        return list;
+    }
+
+    private Map<String, UserInfo> getTestMapData() {
+        Map<String, UserInfo> map = new HashMap<>();
+        getTestListData().forEach(item -> {
+            map.put(item.getName(), item);
+        });
+        return map;
+    }
+
+
+    /**
+     * list转map
+     */
     @Test
     public void list2Map() {
-        List<UserInfo> list = new ArrayList<>();
-        list.add(new UserInfo("1", "张三", 29));
-        list.add(new UserInfo("2", "李四", 18));
-        list.add(new UserInfo("2", "王五", 30));
+        List<UserInfo> list = getTestListData();
+
 
         Map<String, UserInfo> map = list.stream()
                 .collect(
@@ -47,6 +69,55 @@ public class CollectionTest {
         // 遍历。参数只有一个item，可以省略（）；输出只有一行，可以省略{}
         map.values().forEach(item -> System.out.println(item.getName()));
     }
+
+
+    /**
+     * 过滤
+     */
+    @Test
+    public void filter() {
+
+        List<UserInfo> list = getTestListData();
+
+        List<UserInfo> filtedList = list.stream().filter(user -> user.getAge() > 18).collect(Collectors.toList());
+
+        filtedList.forEach(userInfo -> System.out.println(userInfo.getName()));
+    }
+
+
+    /**
+     * foreach遍历map
+     */
+    @Test
+    public void foreachMap() {
+        Map<String, UserInfo> map = getTestMapData();
+        map.forEach((key, value) -> {
+            System.out.println("key:" + key + " value:" + value);
+        });
+    }
+
+
+    /**
+     * 测试group by
+     */
+    @Test
+    public void groupingBy() {
+        List<UserInfo> list = getTestListData();
+        Map<String, List<UserInfo>> collect = list.stream().collect(
+                Collectors.groupingBy(
+                        userInfo -> userInfo.getCity()
+                )
+        );
+
+        collect.forEach((k, v) -> {
+            System.out.println("k:" + k + " v:" + v);
+        });
+
+
+    }
+
+
+
 
 
 }
