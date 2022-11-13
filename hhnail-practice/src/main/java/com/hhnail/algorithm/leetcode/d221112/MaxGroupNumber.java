b7686deacdcda3a80781d0c66082d08368f298d8
Hhnail <779943749@qq.com>
2022-11-13 10:53:29 +0800

    MaxGroupNumber leetcode

---
+++ hhnail-practice/src/main/java/com/hhnail/algorithm/leetcode/d221112/MaxGroupNumber.java
@@ -0,0 +1,16 @@
+package com.hhnail.algorithm.leetcode.d221112;
+
+/**
+ * https://leetcode.cn/problems/Up5XYM/
+ *
+ * LCP 36. 最多牌组数
+ *
+ * 感觉太难了。。。先算了哈哈哈
+ */
+public class MaxGroupNumber {
+
+    public int maxGroupNumber(int[] tiles) {
+
+        return -1;
+    }
+}
