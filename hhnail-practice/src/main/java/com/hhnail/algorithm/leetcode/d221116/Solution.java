b944dd0d0a596faa6161e939c28968da3f60904a
penghangwei <hhnail@163.com>
2022-11-16 18:40:07 +0800

    leetcode-最长公共前缀；List中使用String.join Test

---
+++ hhnail-practice/src/main/java/com/hhnail/algorithm/leetcode/d221116/Solution.java
@@ -0,0 +1,64 @@
+package com.hhnail.algorithm.leetcode.d221116;
+
+import java.util.*;
+
+// 最长公共前缀：编写一个函数来查找字符串数组中的最长公共前缀。
+// https://leetcode.cn/problems/longest-common-prefix/description/
+// 如果不存在公共前缀，返回空字符串 ""。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：strs = ["flower","flow","flight"]
+//输出："fl"
+//
+//
+// 示例 2：
+//
+//
+//输入：strs = ["dog","racecar","car"]
+//输出：""
+//解释：输入不存在公共前缀。
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] 仅由小写英文字母组成
+//
+// Related Topics 字符串
+// 👍 2551 👎 0
+public class Solution {
+
+    public static void main(String[] args) {
+        // String[] strs = new String[]{"flower", "flow", "flight"};
+        String[] strs = new String[]{"a"};
+        System.out.println(new Solution().longestCommonPrefix(strs));
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 1){
+            return strs[0];
+        }
+
+        int maxLength = Arrays.stream(strs).min(Comparator.comparing(String::length)).get().length();
+        StringBuffer longestCommonPrefix = new StringBuffer();
+        for (int i = 0; i < maxLength; i++) {
+            for (int j = 0; j < strs.length - 1; j++) {
+                char c1 = strs[j].charAt(i);
+                char c2 = strs[j + 1].charAt(i);
+                if (c1 != c2) {
+                    return longestCommonPrefix.toString();
+                }
+                longestCommonPrefix.append(j == strs.length - 2 ? c1 : "");
+
+            }
+        }
+        return longestCommonPrefix.toString();
+    }
+}
---
+++ hhnail-practice/src/main/java/com/hhnail/test/ListTest.java
@@ -0,0 +1,36 @@
+package com.hhnail.test;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ListTest {
+
+    @Test
+    public void testJoin() {
+        List<String> employeeNoList = new ArrayList<>();
+        employeeNoList.add("v1");
+        employeeNoList.add("v2");
+        employeeNoList.add("v3");
+        String robam_users = employeeNoList.stream().collect(Collectors.joining(","));
+        System.out.println(robam_users);
+    }
+
+    @Test
+    public void testJoin2() {
+        List<Map<String, Object>> employeeNoList = new ArrayList<>();
+        for (int i = 1; i < 4; i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("employeeNo", "v" + i);
+            employeeNoList.add(map);
+        }
+
+
+    }
+
+
+}
