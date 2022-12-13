12fa2a4d87a9be713f5cc2914c5c179508fcb872
penghangwei <hhnail@163.com>
2022-12-13 17:08:27 +0800

    测试long的最大值有多长

---
+++ hhnail-practice/src/main/java/com/hhnail/test/base/LongTest.java
@@ -0,0 +1,17 @@
+package com.hhnail.test.base;
+
+import org.junit.Test;
+
+/**
+ * @author r221587
+ * @version 1.0
+ * @description: 测试long
+ * @date 2022/12/13 16:46
+ */
+public class LongTest {
+
+    @Test
+    public void test(){
+        System.out.println(String.valueOf(Long.MAX_VALUE).length());
+    }
+}
