5c26bc453cbc96c9d1babfa6bcfda6ccb0a2f541
penghangwei <hhnail@163.com>
2022-12-21 16:57:42 +0800

    commit

--- hhnail-practice/src/main/java/com/hhnail/design/pattern/creator/strategy/TestComparator.java
+++ hhnail-practice/src/main/java/com/hhnail/design/pattern/creator/strategy/TestComparator.java
@@ -2,35 +2,49 @@ package com.hhnail.design.pattern.creator.strategy;
 
 import java.util.Arrays;
 
+/**
+ * 策略模式
+ * 泛型
+ */
 public class TestComparator {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Cat[] cats = {
-				new Cat(12d, 4),
-				new Cat(11d, 2),
-				new Cat(15d, 5),
-				new Cat(13.5,4)
-		};
-		System.out.println(Arrays.toString(cats));
-		System.out.println("===============================");
-		HSorter<Cat> catSorter = new HSorter<>();
-		// 按照age排序
-		// catSorter.sort(cats, new CatAgeComparator());
-		// System.out.println(Arrays.toString(cats));
-
-		// 按照speed排序，并使用lambda表达式（因为接口只有一个抽象方法，可以省略为lambda表达式）
-		catSorter.sort(cats, (o1, o2) -> {
-			if (o1.getSpeed() - o2.getSpeed() < 0) {
-				return -1;
-			} else if (o1.getSpeed() > o2.getSpeed()) {
-				return 1;
-			}
-			return 0;
-		});
-		System.out.println(Arrays.toString(cats));
+        Cat[] cats = {
+                new Cat(12d, 4),
+                new Cat(11d, 2),
+                new Cat(15d, 5),
+                new Cat(13.5, 4)
+        };
+        System.out.println("原始排序");
+        System.out.println(Arrays.toString(cats));
 
 
-	}
+        System.out.println("===============华丽的分割线================");
+
+
+        HSorter<Cat> catSorter = new HSorter<>();
+
+        System.out.println("按照age排序");
+        catSorter.sort(cats, new CatAgeComparator());
+        System.out.println(Arrays.toString(cats));
+
+        System.out.println("===============华丽的分割线================");
+
+
+        // 按照speed排序，并使用lambda表达式（因为接口只有一个抽象方法，可以省略为lambda表达式）
+        catSorter.sort(cats, (o1, o2) -> {
+            if (o1.getSpeed() - o2.getSpeed() < 0) {
+                return -1;
+            } else if (o1.getSpeed() > o2.getSpeed()) {
+                return 1;
+            }
+            return 0;
+        });
+        System.out.println("按照速度排序");
+        System.out.println(Arrays.toString(cats));
+
+
+    }
 
 }
--- hhnail-practice/src/main/java/com/hhnail/test/EncryptTest.java
+++ hhnail-practice/src/main/java/com/hhnail/test/EncryptTest.java
@@ -15,6 +15,22 @@ public class EncryptTest {
     @Test
     public void test01(){
         System.out.println(DigestUtil.sha1("7ba1eab3ed7b5e8elb"));
-
     }
+
+    @Test
+    public void test02(){
+        String time = "2022-12-12 14:22:22";
+        String ehrNo = "221587";
+        String myToken = DigestUtil.md5Hex(time + "|huanke|" + ehrNo + "|qyreqorqor");
+        System.out.println(myToken);
+    }
+
+    @Test
+    public void test03(){
+        String time = "2022-12-12 14:22:22";
+        String ehrNo = "schedule";
+        String myToken = DigestUtil.md5Hex(time + "|huanke|" + ehrNo + "|qyreqorqor");
+        System.out.println(myToken);
+    }
+
 }
