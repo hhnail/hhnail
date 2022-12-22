302bf5206287e0aecee4801cf7a429bd4e0d778a
penghangwei <hhnail@163.com>
2022-12-22 10:47:42 +0800

    sha1加密

--- hhnail-practice/src/main/java/com/hhnail/test/EncryptTest.java
+++ hhnail-practice/src/main/java/com/hhnail/test/EncryptTest.java
@@ -19,18 +19,20 @@ public class EncryptTest {
 
     @Test
     public void test02(){
+        // String time = "2022-12-12 14:22:22";
+        // String ehrNo = "221587";
+        // String myToken = DigestUtil.md5Hex(time + "|huanke|" + ehrNo + "|qyreqorqor");
+        // System.out.println(myToken); // 0fa764dba0d698df20d221ad77d201dd
+
         String time = "2022-12-12 14:22:22";
-        String ehrNo = "221587";
+        String ehrNo = "schedule";
         String myToken = DigestUtil.md5Hex(time + "|huanke|" + ehrNo + "|qyreqorqor");
-        System.out.println(myToken);
+        System.out.println(myToken); // a1334dff4882477d0f4c0648abb41718
     }
 
     @Test
     public void test03(){
-        String time = "2022-12-12 14:22:22";
-        String ehrNo = "schedule";
-        String myToken = DigestUtil.md5Hex(time + "|huanke|" + ehrNo + "|qyreqorqor");
-        System.out.println(myToken);
+        System.out.println(DigestUtil.sha1("7ba1eab3ed7b5e8elb"));
     }
 
 }
