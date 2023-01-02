749a39f43d28bbd341c7d70be55729c8958c90e7
Hhnail <779943749@qq.com>
2023-01-02 10:31:24 +0800

    RabbitMQ-简单模式

---
+++ hhnail-practice/src/main/java/com/hhnail/test/rabbitmq/Consumer.java
@@ -0,0 +1,40 @@
+package com.hhnail.test.rabbitmq;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+/**
+ * mq
+ * 简单模式
+ * 消费者代码
+ */
+public class Consumer {
+
+    public static final String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        // MQ服务地址。这里用的是虚拟机的MQ。端口默认15672
+        factory.setHost("192.168.225.130");
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.basicConsume(
+                QUEUE_NAME, // 队列名
+                true, // 是否自动应答
+                // 成功的回调
+                (consumerTag, message) -> {
+                    byte[] body = message.getBody();
+                    System.out.println("消费成功");
+                    System.out.println(new String(body));
+                },
+                // 失败时的回调
+                consumerTag -> System.out.println("消费失败：" + consumerTag)
+        );
+
+
+    }
+}
---
+++ hhnail-practice/src/main/java/com/hhnail/test/rabbitmq/Producer.java
@@ -0,0 +1,39 @@
+package com.hhnail.test.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * mq
+ * 简单模式
+ * 生产者代码
+ */
+public class Producer {
+
+    public static final String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        // MQ服务地址。这里用的是虚拟机的MQ。端口默认15672
+        factory.setHost("192.168.225.130");
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        // 获取连接
+        Connection connection = factory.newConnection();
+        // 创建信道
+        Channel channel = connection.createChannel();
+        // 创建队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        String message = "hello world 123";
+
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+
+
+    }
+
+
+}
