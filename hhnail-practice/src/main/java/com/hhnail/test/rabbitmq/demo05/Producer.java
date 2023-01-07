69d537d626df1187d38315df2b45de2c0cb53e10
Hhnail <779943749@qq.com>
2023-01-07 19:31:54 +0800

    RabbitMQ单个、批量确认；不公平分发；预取值

---
+++ hhnail-practice/src/main/java/com/hhnail/test/rabbitmq/demo05/Producer.java
@@ -0,0 +1,49 @@
+package com.hhnail.test.rabbitmq.demo05;
+
+import com.hhnail.test.rabbitmq.util.RabbitMQUtil;
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+
+/**
+ * @author Hhnail
+ * @version 1.0
+ * @description: 批量确认发布
+ * 生产者
+ * @date 2023/1/7 19:16
+ */
+public class Producer {
+
+    public static final String QUEUE_NAME = "queueName";
+
+    public static final Integer MESSAGE_COUNT = 100;
+    public static final Integer BATCH_CONFIRM = 100;
+
+    public static void main(String[] args) throws Exception {
+        Channel channel = RabbitMQUtil.getChannel();
+        channel.queueDeclare(QUEUE_NAME,
+                true, false, false,
+                null
+        );
+        // 开启发布确认
+        channel.confirmSelect();
+        long start = System.currentTimeMillis();
+        for (Integer i = 0; i < MESSAGE_COUNT; i++) {
+            String message = "消息" + i;
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+            {
+                if (i % BATCH_CONFIRM == 0) {
+                    boolean success = channel.waitForConfirms();
+                }
+            }
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("单个确认发布耗时：" + (end - start));
+
+
+    }
+
+}
---
+++ hhnail-practice/src/main/java/com/hhnail/test/rabbitmq/demo06/Consumer.java
@@ -0,0 +1,44 @@
+package com.hhnail.test.rabbitmq.demo06;
+
+import com.hhnail.test.rabbitmq.util.RabbitMQUtil;
+import com.rabbitmq.client.Channel;
+
+/**
+ * @author Hhnail
+ * @version 1.0
+ * @description: 消费者
+ * 不公平分发、预取值
+ * @date 2023/1/7 19:28
+ */
+public class Consumer {
+
+
+    public static final String QUEUE_NAME = "ack_message";
+
+    public static void main(String[] args) throws Exception {
+
+        Channel channel = RabbitMQUtil.getChannel();
+
+        { // TODO 核心代码
+            int prefetchCount = 1; // 不公平分发
+            // int prefetchCount = 5; // 预取值
+            channel.basicQos(prefetchCount);
+        }
+        boolean autoAsk = false;
+        channel.basicConsume(
+                QUEUE_NAME, // 队列名
+                autoAsk, // 是否自动应答
+                // 成功的回调
+                (consumerTag, message) -> {
+                    byte[] body = message.getBody();
+                    System.out.println("消费成功");
+                    System.out.println(new String(body));
+                    // 手动应答
+                    channel.basicAck(message.getEnvelope().getDeliveryTag(), false);
+                },
+                // 失败时的回调
+                consumerTag -> System.out.println("消费失败：" + consumerTag)
+        );
+
+    }
+}
