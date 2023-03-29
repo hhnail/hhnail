bbcad8d50790b87d7d061882f2f9629a54fc279a
Hhnail <hhnail@163.com>
2023-03-29 16:36:39 +0800

    Map test

---
+++ hhnail-practice/src/main/java/com/hhnail/test/base/MapTest.java
@@ -0,0 +1,34 @@
+package com.hhnail.test.base;
+
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author r221587
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/3/29 15:54
+ */
+public class MapTest {
+
+    @Test
+    public void print() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("k1", "v1");
+        map.put("k2", "v2");
+        map.put("k3", "v3");
+
+        Map<String, Object> map2 = new HashMap<>();
+        map2.put("k1", "v1");
+        map2.put("k2", "v2");
+        map2.put("k3", "v3");
+
+        map.put("map", map2);
+
+        System.out.println("log:" + map);
+        System.out.println("json log:" + JSONObject.toJSONString(map));
+    }
+}
