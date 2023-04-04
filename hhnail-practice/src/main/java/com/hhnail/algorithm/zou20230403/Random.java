04fcdc4faf58eaf17cc27b18588f7532c96bc4a6
Hhnail <hhnail@163.com>
2023-04-04 15:45:28 +0800

    算法-左程云-将1~5的等概率随机整数 转为 1~7的等概率随机整数

--- hhnail-practice/src/main/java/com/hhnail/algorithm/zou20230403/Random.java
+++ hhnail-practice/src/main/java/com/hhnail/algorithm/zou20230403/Random.java
@@ -46,6 +46,31 @@ public class Random {
         System.out.println((double) count / (double) testTimes);
         System.out.println((double) count2 / (double) testTimes);
 
+
+        System.out.println("=================== 华丽的分割线 ====================");
+        k = 7;
+        testTimes = 700000;
+        arr = new int[k];
+        for (int i = 0; i < testTimes; i++) {
+            arr[f3()]++;
+        }
+        for (int i = 0; i < k; i++) {
+            System.out.println(i + "出现的次数为：" + arr[i]);
+        }
+
+        System.out.println("=================== 华丽的分割线 ====================");
+        k = 7;
+        testTimes = 700000;
+        arr = new int[k];
+        for (int i = 0; i < testTimes; i++) {
+            int random = f4();
+            // System.out.println(random);
+            arr[random - 1]++;
+        }
+        for (int i = 0; i < k; i++) {
+            System.out.println(i + 1 + "出现的次数为：" + arr[i]);
+        }
+
     }
 
     // 随机1、2、3、4、5
@@ -70,6 +95,24 @@ public class Random {
             random = f1();
         } while (random == 3);
         return random < 3 ? 0 : 1;
+    }
+
+
+    // 随机0、1——>随机0~6的整数
+    private static int f3() {
+        int r1 = f2() << 2;
+        int r2 = f2() << 1;
+        int r3 = f2() << 0;
+        int random = r1 + r2 + r3;
+        if (random == 7) {
+            return f3();
+        }
+        return random;
+    }
+
+    // 随机0、1——>随机1~7的整数
+    private static int f4() {
+        return f3() + 1;
     }
 
 
