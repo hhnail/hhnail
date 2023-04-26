d8182e2f83873f36f3b13f62321836601ffacd1f
Hhnail <hhnail@163.com>
2023-04-26 11:35:06 +0800

    xxl-job测试

--- hhnail-restful/src/main/java/com/hhnail/web/controller/UserController.java
+++ hhnail-restful/src/main/java/com/hhnail/web/controller/UserController.java
@@ -2,30 +2,47 @@ package com.hhnail.web.controller;
 
 import com.hhnail.web.bean.User;
 import com.hhnail.web.service.UserService;
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+// @Api(tags = "用户模块")
 @RestController
 @Slf4j
-// @Api(tags = "用户模块")
+// @Component
 public class UserController {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
     @ApiOperation("查询用户")
-	@GetMapping("/queryUser")
-	public List<User> queryUser() {
-		List<User> users = userService.queryUser();
-	    System.out.println("users=" + users.toString());
-		log.debug("users={}", users);
-		return users;
-	}
+    @PostMapping("/queryUser")
+    @XxlJob("queryUser")
+    public ReturnT<String> queryUser(String param) {
+        List<User> users = userService.queryUser();
+        System.out.println("users=" + users.toString());
+        log.debug("users={}", users);
+        // return users;
+        return ReturnT.SUCCESS;
+    }
+
+    @PostMapping("/run")
+    @XxlJob("queryUser")
+    public ReturnT<String> run(String param) {
+        List<User> users = userService.queryUser();
+        System.out.println("users=" + users.toString());
+        log.debug("users={}", users);
+        // return users;
+        return ReturnT.SUCCESS;
+    }
 
 
 }
---
+++ hhnail-restful/src/main/java/com/hhnail/web/handler/XxlJobHandler.java
@@ -0,0 +1,29 @@
+package com.hhnail.web.handler;
+
+import com.hhnail.web.bean.User;
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author r221587
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/4/26 11:08
+ */
+@Component
+@Slf4j
+public class XxlJobHandler {
+
+    @XxlJob("println")
+    public ReturnT<String> queryUser(String param) {
+        // List<User> users = userService.queryUser();
+        System.out.println("定时任务执行了==========================");
+        log.debug("定时任务执行了==========================");
+        // return users;
+        return ReturnT.SUCCESS;
+    }
+}
