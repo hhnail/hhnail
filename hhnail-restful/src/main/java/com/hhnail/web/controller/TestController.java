2290bd4e7ba2952538c60b5829e7f717108e778d
Hhnail <hhnail@163.com>
2023-05-03 14:24:48 +0800

    ChatGPT实现分布式锁

---
+++ hhnail-restful/src/main/java/com/hhnail/web/controller/TestController.java
@@ -0,0 +1,44 @@
+package com.hhnail.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author ChatGPT
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/5/3 14:19
+ */
+@RestController
+@Slf4j
+public class TestController {
+
+
+    private static final String LOCK_KEY = "lock_key";
+
+    private static final int LOCK_EXPIRE_TIME = 5000;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public boolean acquireDistributedLock(String requestId) {
+        Boolean success = redisTemplate.opsForValue().setIfAbsent(
+                LOCK_KEY, requestId, LOCK_EXPIRE_TIME, TimeUnit.MILLISECONDS
+        );
+        return success;
+
+    }
+
+    public boolean releaseDistributedLock(String requestId) {
+        Object lockValue = redisTemplate.opsForValue().get(LOCK_KEY);
+        if (requestId.equals(lockValue)) {
+            redisTemplate.delete(LOCK_KEY);
+            return true;
+        }
+        return false;
+    }
+}
