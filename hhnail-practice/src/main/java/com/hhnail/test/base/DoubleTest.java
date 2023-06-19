c7c4fd4246ce39ac5fc12ec70ebe59af2d660492
Hhnail <hhnail@163.com>
2023-06-19 13:12:31 +0800

    测试Double，空字符串

---
+++ hhnail-practice/src/main/java/com/hhnail/test/base/DoubleTest.java
@@ -0,0 +1,15 @@
+package com.hhnail.test.base;
+
+/**
+ * @author r221587
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/6/8 17:29
+ */
+public class DoubleTest {
+
+    public static void main(String[] args) {
+        System.out.println(Double.parseDouble("0"));
+        System.out.println(Double.parseDouble(""));
+    }
+}
