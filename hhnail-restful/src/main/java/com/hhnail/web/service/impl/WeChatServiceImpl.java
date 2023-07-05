5e1dd0166d9fae3c359d54f0f307e9c11697b295
Hhnail <hhnail@163.com>
2023-07-05 08:49:26 +0800

    拉取微信公众号用户列表-多线程执行-v1未完成

--- hhnail-restful/src/main/java/com/hhnail/web/service/impl/WeChatServiceImpl.java
+++ hhnail-restful/src/main/java/com/hhnail/web/service/impl/WeChatServiceImpl.java
@@ -14,11 +14,23 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class WeChatServiceImpl implements IWeChatService {
+
+    // 创建线程池并设置参数
+    private int corePoolSize = 10;
+    private int maxPoolSize = 30;
+    private long keepAliveTime = 5000;
+    private TimeUnit timeUnit = TimeUnit.MILLISECONDS;
+    private BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>();
+    private ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(corePoolSize, maxPoolSize, keepAliveTime, timeUnit, workQueue);
 
     @Autowired
     WeChatMapper weChatMapper;
@@ -65,13 +77,43 @@ public class WeChatServiceImpl implements IWeChatService {
 
     @Override
     public List<WeChatFollower> saveWeChatFollowerList(Map<String, Object> reqVO) {
-        List<WeChatFollower> weChatFollowerList = new ArrayList<>();
+        boolean flag = true;
+        final JSONArray jsonArray = new JSONArray();
+        String next_openid = null;
+        // String next_openid = "o1sGt1Z0A-SqnnaYQjnIxyuhVb-g";
+        while (flag) {
+            String accessToken = getAccessToken(reqVO.get("appid"), reqVO.get("secret"));
+            // String accessToken = "70_RJzKwEmFTxq1z_1kS_xVE_A2qCh1Hi0zlyfnSwso4jr8qHbn1YnpkqmDfpeuSFyb3sCv2CPut_1ocXbqZgwXoE6lAqcFnUIgPbJvPrgGJ-CiN6lJVbD4GqBeasAVHAbAIAEQJ";
+            String openidListUrl = "https://api.weixin.qq.com/cgi-bin/user/get" +
+                    "?access_token=" + accessToken +
+                    (next_openid == null ? "" : ("&next_openid=" + next_openid));
+            String reqResult = HttpUtil.get(openidListUrl);
+            JSONObject result = JSONObject.parseObject(reqResult);
+            JSONArray openidArr = result.getJSONObject("data").getJSONArray("openid");
+            next_openid = openidArr.get(openidArr.size() - 1).toString();
 
-        String openidListUrl = "https://api.weixin.qq.com/cgi-bin/user/get" +
-                "?access_token=" + getAccessToken(reqVO.get("appid"), reqVO.get("secret"));
-        String reqResult = HttpUtil.get(openidListUrl);
-        JSONObject result = JSONObject.parseObject(reqResult);
-        JSONArray jsonArray = result.getJSONObject("data").getJSONArray("openid");
+            if (openidArr.size() < 10000) {
+                flag = false;
+            }
+
+            jsonArray.clear();
+            jsonArray.addAll(openidArr);
+            threadPoolExecutor.execute(() -> {
+                // 执行相同任务
+                try {
+                    JSONArray arr = jsonArray;
+                    saveDataBatch(reqVO, arr);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        return null;
+    }
+
+    private void saveDataBatch(Map<String, Object> reqVO, JSONArray jsonArray) {
+        String reqResult;
         for (Object openid : jsonArray) {
             String userInfoUrl = "https://api.weixin.qq.com/cgi-bin/user/info" +
                     "?access_token=" + getAccessToken(reqVO.get("appid"), reqVO.get("secret")) +
@@ -80,10 +122,9 @@ public class WeChatServiceImpl implements IWeChatService {
             reqResult = HttpUtil.get(userInfoUrl);
             WeChatFollower weChatFollower = JSONObject.parseObject(reqResult, WeChatFollower.class);
             weChatFollower.setTagIdListStr(weChatFollower.getTagIdList().stream().collect(Collectors.joining(",")));
+            System.out.println("currentThread------------------:" + Thread.currentThread().getName());
             System.out.println("current:" + weChatFollower);
             weChatMapper.insert(weChatFollower);
-            weChatFollowerList.add(weChatFollower);
         }
-        return weChatFollowerList;
     }
 }
