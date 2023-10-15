20a1f35384aff95a173f80e37da18b03afeaeb63
Hhnail <779943749@qq.com>
2023-10-15 11:00:52 +0800

    测试Copilot（用不了）

---
+++ hhnail-practice/src/main/java/com/hhnail/test/CopilotTest.java
@@ -0,0 +1,29 @@
+package com.hhnail.test;
+
+import java.time.LocalDate;
+
+/**
+ * @author Hhnail
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/9/24 10:18
+ */
+public class CopilotTest {
+
+    public static void main(String[] args) {
+
+        // 正则表达式校验手机号码
+
+
+
+
+
+        LocalDate currentDate = LocalDate.now();
+        LocalDate threeDaysBefore = currentDate.minusDays(3);
+        System.out.println("3天前的日期: " + threeDaysBefore);
+
+
+
+    }
+
+}
